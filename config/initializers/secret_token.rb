e3ade438eda94abfb6d2774d5193d0611c04b4d1
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-04-14 12:38:39 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeFoodRIT::Application.config.secret_token = 'd21fd86a7c790306cdbdeb730926153deaf7594d7ac35028382b006b40f177833ea35c18043f3a28d739a0bfc0f26fe7b57f37ca92ffc546200c3197d22d11d5'
