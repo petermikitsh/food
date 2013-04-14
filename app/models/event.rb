c7db1b6ad9a9b9337c58d5e074a5b06a2b58ae3b
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-04-14 18:41:03 -0400

    Added location to events form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :end, :start, :title
   has_many :foods
-  has_one :location
+  belongs_to :location
+  accepts_nested_attributes_for :location
   validates :end, :start, :title, :presence => true
 end
