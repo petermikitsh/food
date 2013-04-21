849a322a188bceddd9f84d87ef2e0c0164642f8c
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-04-21 15:02:45 -0400

    search by date on map controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,18 @@
 class MapController < ApplicationController
   def index
-    @json = Location.all.to_gmaps4rails do |location, marker|
-    	marker.infowindow "Hello World"
+
+  	if (params[:start_date] && !params[:start_date].empty? &&
+  		params[:end_date] && !params[:end_date].empty?)
+  		@q_start_date = Date.strptime(params[:start_date], "%m/%d/%Y")
+  		@q_end_date = Date.strptime(params[:end_date], "%m/%d/%Y")
+		@query = Event.where(:start_date => (@q_start_date..@q_end_date))
+	else
+		@query = Event.where(:start_date => Date.today)
+	end
+    
+    @json = @query.all.to_gmaps4rails do |event, marker|
+    	marker.infowindow event.title
     end
+
   end
 end
