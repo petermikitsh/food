666a10962a026b6903c1f3cc6e92b4f4f3e3ebd6
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-04-26 19:31:03 -0400

    Fixed date format bug; disabled map auto_adjust

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,13 +8,15 @@ class MapController < ApplicationController
 		@query = Event.where(:start_date => (@q_start_date..@q_end_date))
 	else
 		@query = Event.where(:start_date => Date.today)
+    @q_start_date = Date.today
+    @q_end_date = Date.today
 	end
 
   if (@query == [])
     @notice = 'No events found for the specified time frame.'
   end
 
-  @json = @query.all.to_gmaps4rails do |event, marker|
+  @json = @query.to_gmaps4rails do |event, marker|
   	marker.infowindow render_to_string :partial => "marker",
                                        :locals => {:query => @query.where(:location_id => event.location) }
   end
