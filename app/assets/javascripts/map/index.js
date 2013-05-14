3ebafa9bd076706e537519526021e3c066ba4b6e
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-05-13 23:15:18 -0400

    add chevrons to map index accordion

--- app/assets/javascripts/map/index.js
+++ app/assets/javascripts/map/index.js
@@ -15,4 +15,14 @@ $(document).ready(function() {
   		$('#modal_title').append("Post to " + site);
   		$('#modal_body').append("Free Food at " + message + "!");
 	});
+  $('.accordion').on('show', function (e) {
+    getButton(e).removeClass("icon-chevron-right").addClass("icon-chevron-down");
+  });
+  $('.accordion').on('hide', function (e) {
+    getButton(e).removeClass("icon-chevron-down").addClass("icon-chevron-right");
+  });
 });
+
+function getButton(e) {
+  return $(e.target).prev('.accordion-heading').find('.accordion-button');
+}
